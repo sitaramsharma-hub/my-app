fed3a53ebcf40c2ccf18abe35f268d4295b7adf2
sitaramsharma-hub <79745791+sitaramsharma-hub@users.noreply.github.com>
2025-11-07 05:40:55 +0530

    Add TextUtils app with routing and UI components
    Introduces a React app for text manipulation with dark/light mode, alert notifications, and Bootstrap styling. Adds routing via react-router-dom, new components (Navbar, TextForm, Alert, About), updates favicon assets, and enhances index.html for improved branding and Bootstrap integration.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import React,{useState} from 'react';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState('light');
+  const[alert,setAlert] = useState(null); 
+  const showAlert = (message,type) => {
+      setAlert({
+        msg : message,
+        type : type
+      })
+      setTimeout(()=>{
+      setAlert(null)
+      },1500)
+  }
+  const toggleMode = () => {
+    console.log(mode);
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", 'success');
+      document.title = 'TextUtils - Dark Mode';
+      /*setInterval(()=>{
+        document.title = 'TextUtils is Amazing Mode';
+      },2000);
+      setInterval(()=>{
+        document.title = 'Install TextUtils Now';
+      },3000);*/
+    }else{
+      setMode('light');
+       document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled", 'success');
+        document.title = 'TextUtils - Light Mode';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>  
+  <Router> 
+ <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode}/>
+ <Alert alert={alert}/>
+ <div className='container my-3'>
+  <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter text to analyze" mode={mode} />} />
+  </Routes> 
+ </div>
+ </Router> 
+  </>
   );
 }
 
-export default App;
+export default App; 
